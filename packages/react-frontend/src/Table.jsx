f442913ed963ddc06aa0d3f70c89e7e40de08c0b
rrodr <rrodr195@calpoly.edu>
2026-08-31 13:48:58 -0700

    IE1 step 6 comit msg

---
+++ packages/react-frontend/src/Table.jsx
@@ -0,0 +1,52 @@
+// src/Table.jsx
+import React from "react"
+
+function TableHeader() {
+  return ( 
+	  <thead>
+	    <tr>
+	      <th>Name</th>
+	      <th>Job</th>
+	    </tr>
+	  </thead>
+  );
+
+}
+
+function TableBody(props) {
+  const rows = props.characterData.map((row, index) => {
+    return ( 
+	    <tr key={index}>
+	      <td>{row.name}</td>
+	      <td>{row.job}</td>
+	      <td> 
+	        <button OnClick={() => props.removeCharacter(index)}>Delete</button>
+	  
+	      </td>
+	    </tr>
+    );
+  }
+ );
+	return (
+		<tbody> 
+		  {rows}
+		</tbody>
+
+  
+	);
+}
+
+function Table(props) {
+  return (
+	  <table>
+	    <TableHeader />
+	    <TableBody 
+	      characterData={props.characterData}
+	      removeCharacter={props.removeCharacter}
+	    />
+	  </table>
+  );
+
+}
+
+export default Table;
